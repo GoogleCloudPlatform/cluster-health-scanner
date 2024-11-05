54e6ee7086668e5c8898b500ef99bb0044d6fe91
NinaCai <ninacai_google_com@a3mega-controller.us-central1-c.c.hpc-toolkit-gsc.internal>
2024-11-05 22:40:19 +0000

    change vars to env vars

--- src/gpu_healthcheck/gpu_healthcheck.sh
+++ src/gpu_healthcheck/gpu_healthcheck.sh
@@ -5,10 +5,11 @@
 #SBATCH --gpus-per-node 8
 #SBATCH --nodes 1
 
-NODE_NAME=$HOSTNAME
-HEALTH_VALIDITY_HOURS=24
-DRY_RUN=true
-R_LEVEL=2
+export NODE_NAME=$HOSTNAME
+echo "Running on node: $NODE_NAME"
+export HEALTH_VALIDITY_HOURS=24
+export DRY_RUN=true
+export R_LEVEL=2
 
 # Launch the script
 srun --container-image=./gpu+slurm.sqsh bash -c "python3 /app/gpu_healthcheck.py"
