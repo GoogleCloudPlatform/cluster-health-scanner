3b2665326caa8ec0f0f6d4e80922c02a280fb0a8
NinaCai <ninacai_google_com@a3mega-controller.us-central1-c.c.hpc-toolkit-gsc.internal>
2024-11-06 16:33:54 +0000

    use correct dcgm base image

--- src/gpu_healthcheck/gpu_healthcheck.sh
+++ src/gpu_healthcheck/gpu_healthcheck.sh
@@ -11,5 +11,12 @@ export HEALTH_VALIDITY_HOURS=24
 export DRY_RUN=true
 export R_LEVEL=2
 
+#: "${VERSION:=slurm}"
+#srun docker build -t gpu:"${VERSION}" .
+#srun rm -f gpu+"${VERSION}".sqsh
+#srun enroot import dockerd://gpu:"${VERSION}"
+
+CONTAINER_MOUNTS="/etc/slurm/:/etc/slurm/"
+
 # Launch the script
-srun --container-image=./gpu+slurm.sqsh bash -c "python3 /app/gpu_healthcheck.py"
+srun --container-image=./gpu+slurm.sqsh --container-mounts="${CONTAINER_MOUNTS}" bash -c "python3 /app/gpu_healthcheck.py"
