682679cefb446ddfa027e6e6b71d13bac37239fd
NinaCai <ninacai_google_com@a3mega-controller.us-central1-c.c.hpc-toolkit-gsc.internal>
2024-11-07 15:50:35 +0000

    fix container mount

--- src/gpu_healthcheck/gpu_healthcheck.sh
+++ src/gpu_healthcheck/gpu_healthcheck.sh
@@ -11,12 +11,15 @@ export HEALTH_VALIDITY_HOURS=24
 export DRY_RUN=true
 export R_LEVEL=2
 
+export USER_ID=0 #"$(id -u)"
+export GROUP_ID=0 #"$(id -g)"
 #: "${VERSION:=slurm}"
 #srun docker build -t gpu:"${VERSION}" .
 #srun rm -f gpu+"${VERSION}".sqsh
 #srun enroot import dockerd://gpu:"${VERSION}"
 
-CONTAINER_MOUNTS="/etc/slurm/:/etc/slurm/"
-
+#CONTAINER_MOUNTS="/etc/slurm/:/etc/slurm/"
+#CONTAINER_MOUNTS="/etc/slurm/:/var/spool/slurmd/conf-cache/"
+CONTAINER_MOUNTS="/usr/local/bin,/usr/local/lib,/var/spool/slurmd,/var/spool/slurm,/var/run/slurm,/etc/passwd:/etc/passwd,/usr/lib64,/var/run/munge"
 # Launch the script
-srun --container-image=./gpu+slurm.sqsh --container-mounts="${CONTAINER_MOUNTS}" bash -c "python3 /app/gpu_healthcheck.py"
+srun --container-image=./gpu+slurm.sqsh --container-mounts="${CONTAINER_MOUNTS}"  bash -c "python3 /app/gpu_healthcheck.py"
